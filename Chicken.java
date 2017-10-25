2850ae7d09c4525a6322defb3bdbb9bb78c1e082
esrivera <santi_rv@hotmail.com>
2017-10-25 11:13:30 -0500

    Firts version the Chicken class

---
+++ Chicken.java
@@ -0,0 +1,24 @@
+public class Chicken{
+	private String name;
+	private String color;
+	private int age;
+	
+	public void layAnEgg(){
+		System.out.println("the chicken lay an egg");
+	}
+	public void poop(){
+		System.out.println("the chicken pooped");
+	}
+	public void drink(){
+		System.out.println("the chicken drank whater");
+	}
+	public void print(){
+		System.out.println("the name of the chicken is -->"+name+", color-->"+color+", age-->"+age+" ");
+	}
+	public void initializeData(String name,String color,int age){
+		this.name=name;
+		this.color=color;
+		this.age=age;
+	}
+	
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello Santiago");
+	
+	Chicken chicken;
+	chicken=new Chicken();
+	chicken.initializeData("Lucy","Brown",2);
+	chicken.print();
 	}
+	
 }
