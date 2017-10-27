8ebf325fb24bc40bddcf12e2ea6fba53c815f9e6
esrivera <santi_rv@hotmail.com>
2017-10-26 19:43:57 -0500

    second version chicken

--- Chicken.java
+++ Chicken.java
@@ -2,7 +2,7 @@ public class Chicken{
 	private String name;
 	private String color;
 	private int age;
-	
+
 	public void layAnEgg(){
 		System.out.println("the chicken lay an egg");
 	}
@@ -10,15 +10,26 @@ public class Chicken{
 		System.out.println("the chicken pooped");
 	}
 	public void drink(){
-		System.out.println("the chicken drank whater");
+		System.out.println("the chicken drank water");
 	}
+	public void noLayAnEgg(){
+		System.out.println("the chicken didn't lay an egg");
+	}
+	public void noPoop(){
+		System.out.println("the chicken didn't poop");
+	}
+	public void noDrink(){
+		System.out.println("the chicken not drank water");
+	}
+
+
 	public void print(){
-		System.out.println("the name of the chicken is -->"+name+", color-->"+color+", age-->"+age+" ");
+		System.out.println("the name of the chicken is-->"+name+", color-->"+color+", age-->"+age+" ");
 	}
 	public void initializeData(String name,String color,int age){
 		this.name=name;
 		this.color=color;
 		this.age=age;
 	}
-	
+
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,35 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello Santiago");
-	
-	Chicken chicken;
-	chicken=new Chicken();
-	chicken.initializeData("Lucy","Brown",2);
-	chicken.print();
+
+	Chicken chickenOne;
+	chickenOne=new Chicken();
+	chickenOne.initializeData("Lucy","Brown",2);
+	chickenOne.print();
+	chickenOne.layAnEgg();
+	chickenOne.poop();
+	chickenOne.drink();
+	Chicken chickenTwo;
+	chickenTwo=new Chicken();
+	chickenTwo.initializeData("Catalina","Black",3);
+	chickenTwo.print();
+	chickenTwo.layAnEgg();
+	chickenTwo.noPoop();
+	chickenTwo.noDrink();
+	Chicken chickenThree;
+	chickenThree=new Chicken();
+	chickenThree.initializeData("Camila","White",1);
+	chickenThree.print();
+	chickenThree.noLayAnEgg();
+	chickenThree.poop();
+	chickenThree.noDrink();
+	Chicken chickenFour;
+	chickenFour=new Chicken();
+	chickenFour.initializeData("Leticia","Yelow",2);
+	chickenFour.print();
+	chickenFour.noLayAnEgg();
+	chickenFour.noPoop();
+	chickenFour.drink();
 	}
-	
+
 }
